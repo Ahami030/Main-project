ad34abfbd34e05d5601f1517f73f39df0c5e14ba
Ahami030 <140365541+Ahami030@users.noreply.github.com>
2026-06-26 01:30:20 +0700

    fix skeleton loading

--- app/Client/page.tsx
+++ app/Client/page.tsx
@@ -760,7 +760,7 @@ export default function Page(): JSX.Element {
       </div>
 
       {!pageVisible && (
-        <div className="fixed inset-0 z-50 bg-base-200 overflow-y-auto">
+        <div className="fixed inset-0 z-30 bg-base-200 overflow-y-auto pt-16">
           <div className="max-w-6xl mx-auto px-4 md:px-8 py-8 md:py-14 space-y-5">
 
             {/* User card skeleton */}
